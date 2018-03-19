1939a0b3ea011bdd6d186ae6010df9ce3ba7fad7
Katleiah Ramos <katleiahramos@gmail.com>
2018-03-19 19:32:16 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,31 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-     if self.end_with?(".")
-       return true
-     else
-       false
-     end
+     self.end_with?(".")
+
   end
 
   def question?
-    if self.end_with?("?")
-      return true
-    else
-      return false
-    end
+    self.end_with?("?")
+
   end
 
   def exclamation?
-    if self.end_with?("!")
-      true
-    else
-      false
-    end
+    self.end_with?("!")
   end
 
 
   def count_sentences
     self.split(/[.!?]{1,3}/).count
-  end 
+  end
   end
